5cb74b037defa19fe0f7849b78055307342719cd
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-04-21 16:56:41 -0500

    need to focus on db store and apiroutes

---
+++ public/data/store.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const util = require(`util`);
+
+// need to add a class store. which is going to read the db .json file. data your going to have a write and your going to calling get notes add notes and remove.need a promise statement and a stringify statement. 
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,26 +1,35 @@
 const router = require('express').Router()
-const res = require('express/lib/response')
+
 const {indexApiRoutes, filterByQuery} = require('../apiRoutes/index')
 const {indexHtmlRoutes} = require('../htmlRoutes/index')
 
 //Adding the functions to get the notes in html then post the notes 
 router.get('/public/notes.html', (req, res) => {
+    store
     getNotes() 
     .then(notes => {res.json(notes)})
     .catch(err => {
         res.status(`Service Error `)
     })
 });
+
+// need a method add note request from the body and another .then and .catch statement.
 router.post ('/public/notes.html,', (req,res) => {
-    console.log(req.body)
-    .addNote(req.body)
-    .then(note => {
-        res.json(note)
-    })
-    .catch(err => {
-        res.status(`Service Error`)
-    })
+    store
+    req.body.id = filterByQuery.length.toString();
+
+  if (!indexApiRoutes(req.body)) {
+    res.status(400).send("The statement is not properly formatted.");
+  } else {
+    const notes = createNew(req.body, notes);
+    res.json(notes);
+  }
 })
+//create a router delete statement and params id. you will need a remove note there and you need store in your function 
+router.delete() {
+    store 
+}
+
 
 
 module.exports = router;
