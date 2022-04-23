0baba5c66cb53b0c7e52267e873f81d893f9b4f6
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-04-23 11:27:43 -0500

    running but css and javascript not connecting

--- public/data/store.js
+++ public/data/store.js
@@ -4,8 +4,12 @@ const util = require(`util`);
 const readTheFile = util.promisify(fs.readFile);
 const writeTheFile = util.promisify(fs.writeFile);
 
-// need to add a class store. which is going to read the db .json file. data  and your going to have a write and your going to call to get notes add notes and remove. will also need a util.promisify which per my tutor returns a promise instead of a callback statement and I will also need a stringify statement. 
-
+// need to add a uuid which is a npm module that generates identifiers. need to add a class store. which is going to read the db .json file. data  and your going to have a write and your going to call to get notes add notes and remove. will also need a util.promisify which per my tutor returns a promise instead of a callback statement and I will also need a stringify statement. 
+function main() {
+    const uniqueId = uuidv4();
+    console.log(uniqueId);
+}
+// when I envoke the main function it causes the app to get an undefined error and I need to figure that out. 
 
 class dbStore 
 {
